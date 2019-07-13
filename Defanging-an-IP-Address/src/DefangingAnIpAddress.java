97f72ac1c1014213c95f32cab96caf5a7d76e605
TheDevilfromHeaven <52057489+TheDevilfromHeaven@users.noreply.github.com>
2019-07-13 12:16:44 +0800

    IP 地址无效化

--- Defanging-an-IP-Address/src/DefangingAnIpAddress.java
+++ Defanging-an-IP-Address/src/DefangingAnIpAddress.java
@@ -6,17 +6,17 @@ public class DefangingAnIpAddress {
         /*
         * 我的解法就是遍历address，把点改为[.]，然后组合为新的字符串。
         * */
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < address.length(); i++) {
             String ch = address.substring(i, (i + 1));
             if ( ch.equals(".")){
                 ch = "[.]";
-                result += ch;
+                result.append(ch);
             }else {
-                result += ch;
+                result.append(ch);
             }
         }
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args) {
