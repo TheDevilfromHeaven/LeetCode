6a0068e72bda1c5577029583d05c736dadc4ca45
TheDevilfromHeaven <1015157436@qq.com>
2021-02-02 23:13:21 +0800

    1528. 重新排列字符串

---
+++ Java/Shuffle-String/src/ShuffleString.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class ShuffleString {
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[indices.length];
+        for (int i = 0; i < indices.length; i++){
+            result[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(result);
+    }
+
+    public static void main(String[] args) {
+        String s = "art";
+        int[] indices = {1,0,2};
+        ShuffleString shuffleString = new ShuffleString();
+        String s1 = shuffleString.restoreString(s, indices);
+        System.out.println(s1);
+    }
+}
