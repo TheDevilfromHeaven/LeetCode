baf3bf2a5efe98020e7fabe4cc5895a9be9ee5c6
TheDevilfromHeaven <1015157436@qq.com>
2021-02-02 23:15:04 +0800

    1528. 重新排列字符串

--- Java/Shuffle-String/src/ShuffleString.java
+++ Java/Shuffle-String/src/ShuffleString.java
@@ -1,4 +1,5 @@
 public class ShuffleString {
+    //111
     public String restoreString(String s, int[] indices) {
         char[] result = new char[indices.length];
         for (int i = 0; i < indices.length; i++){
